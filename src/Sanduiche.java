bd9cb3c658fe9e3e039c447e4cbde05fc6c6e624
Fernanda Freire <fernandadasilvafreire64@gmail.com>
2026-04-17 20:11:04 -0300

    segue conforme solictado seu requeijão e seu pão de fundo

--- src/Sanduiche.java
+++ src/Sanduiche.java
@@ -1,5 +1,5 @@
 public class Sanduiche {
     public static void main(String[] args) {
-        System.out.println("Pão, salada, carne, molho, picles de pepino");
+        System.out.println("Pão, requeijão, salada, carne, molho, picles de pepino, pão");
     }
 }
